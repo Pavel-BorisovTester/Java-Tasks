b23f824bbdf039fcb74a5b1ff55b3fa2664dacdb
Pavel-BorisovTester <109236734+Pavel-BorisovTester@users.noreply.github.com>
2022-07-17 18:22:22 +0400

    Update main.java

--- .github/workflows/main.java
+++ .github/workflows/main.java
@@ -1,36 +1,106 @@
-# This is a basic workflow to help you get started with Actions
+1.	Печать сумы всех целых чисел от 100 до 500 с отображением операций внутри цикла
+public class HelloWorld{
 
-name: CI
+     public static void main(String []args){
+       int sum = 0;
+       for (int i = 100; i <= 500; i++)
+       {
+        System.out.println("==========================");
+        System.out.println("i = " + i);
+        System.out.println("Before: i = " + sum);
+        System.out.println("Operation: sum =  i + " + sum);
+        sum = sum + i;
+        System.out.println("After: i = " + sum);
+        System.out.println("==========================");
+       }
+       System.out.println("Sum of range is " + sum);
+     }
+}
 
-# Controls when the workflow will run
-on:
-  # Triggers the workflow on push or pull request events but only for the "main" branch
-  push:
-    branches: [ "main" ]
-  pull_request:
-    branches: [ "main" ]
+2.	Посчитать среднее арифметическое вещественных чисел от а до n
+import java.util.Scanner;
+public class HelloWorld{
 
-  # Allows you to run this workflow manually from the Actions tab
-  workflow_dispatch:
+     public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        
+        int n = 8;
+        double sum = 0;
+        int counter = 0;
+        
+        for(int i = 0; i <= n; i++)
+        {
+            double temp = sc.nextDouble();
+            sum = sum + temp;
+            counter++;
+            System.out.println(temp + " ");
+        }
+        System.out.println();
+        System.out.println("Sum is " + sum +"; Count is " + counter);
+        
+        Double avg = sum / counter;
+        System.out.println();
+        System.out.println("Averege is " + avg);
+     }
+}
 
-# A workflow run is made up of one or more jobs that can run sequentially or in parallel
-jobs:
-  # This workflow contains a single job called "build"
-  build:
-    # The type of runner that the job will run on
-    runs-on: ubuntu-latest
+3.	Найти сумму множества целых чисел, которое заканчивается нулём. Найти количество чисел до 0
 
-    # Steps represent a sequence of tasks that will be executed as part of the job
-    steps:
-      # Checks-out your repository under $GITHUB_WORKSPACE, so your job can access it
-      - uses: actions/checkout@v3
+import java.util.Scanner;
+public class HelloWorld{
+     public static void main(String []args){
+         Scanner sc = new Scanner(System.in);
+        int temp = sc.nextInt();
+        int sum = 0;
+        int counter = 0;
 
-      # Runs a single command using the runners shell
-      - name: Run a one-line script
-        run: echo Hello, world!
+          while (temp != 0)
+          {
+              System.out.print(temp + " ");
+              temp = sc.nextInt();
+              sum = sum + temp;
+              counter++;
+          }
+            System.out.print("\n");
+            System.out.print("Sum is " + sum);
+            System.out.print("\n");
+            System.out.print("Count is " + counter);
+     }
+}
 
-      # Runs a set of commands using the runners shell
-      - name: Run a multi-line script
-        run: |
-          echo Add other actions to build,
-          echo test, and deploy your project.
+4.	Напечатать множество чисел с нулем, посчитать сумму множества и счётчик чисел в множестве без 0
+
+import java.util.Scanner;
+public class HelloWorld{
+
+     public static void main(String []args){
+         Scanner sc = new Scanner(System.in);
+         
+        int temp = sc.nextInt();
+        int sum = 0;
+        int counter = 0;
+        int counterZero = 0;
+        
+        
+          while (temp != 0)
+          {
+              System.out.print(temp + " ");
+              temp = sc.nextInt();
+              sum = sum + temp;
+              counter++;
+          }
+              if (temp == 0)
+              {
+                  counterZero++;
+                  System.out.print(temp + " ");
+              }
+              
+            System.out.print("\n");
+            System.out.print("Sum is " + sum);
+            System.out.print("\n");
+            System.out.print("Count is " + counter);
+            System.out.print("\n");
+            System.out.print("Counter of Zero is " + counterZero);
+     }
+}
+
